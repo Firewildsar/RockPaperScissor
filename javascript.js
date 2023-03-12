f3a18954572cabf81ca84861d2bdd4b60630892e
Firewildstar <122087233+Firewildsar@users.noreply.github.com>
2023-03-11 20:46:21 -0800

    Full Functioning Rock Paper Scissor where user inputs Rock, Paper, or Scissor. There is no error when using case sensitive letters. If it is not rock, paper, or scissor, will show not valid.

---
+++ javascript.js
@@ -0,0 +1,60 @@
+console.log("It's working")
+
+/* 
+computer chooses at random rock paper or scissor (1-3) 
+get player selection which would be in string from prompt window
+store player selection as string in playerChoice variable
+store computer choice in variable choice
+
+*/
+
+
+const playerSelection = playerChoice();
+const computerSelection = getComputerChoice();
+
+function playerChoice(){
+    let choice = prompt("Choose Rock, Paper, or Scissor");
+    return choice.toLowerCase();
+}
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*(3)+1)
+    if (choice == 1){
+        return "scissor"
+    } else if (choice == 2){
+        return "rock"
+    } else {
+        return "paper"
+    }
+}
+
+
+
+
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection == "scissor" & computerSelection == "rock"){
+        return "You lose! Rock beats Scissor!"
+    } else if (playerSelection == "rock" & computerSelection == "scissor"){
+        return "You win! Rock beats Scissor!"
+    } else if (playerSelection == "paper" & computerSelection == "scissor"){
+        return "You lose! Scissor beats paper!"
+    } else if (playerSelection == "paper" & computerSelection == "rock"){
+        return "You win! Paper beats rock!"
+    } else if (playerSelection == "rock" & computerSelection == "paper"){
+        return "You lose! Paper beats rock!"
+    } else if (playerSelection == "scissor" & computerSelection == "paper"){
+        return "You win! Scissor beats paper!"
+    } else if (playerSelection == "scissor" & computerSelection == "scissor"){
+        return "You tie! No winners!"
+    } else if (playerSelection == "rock" & computerSelection == "rock"){
+        return "You tie! No winners!"
+    } else if (playerSelection == "paper" & computerSelection == "paper"){
+        return "You tie! No winners!"
+    } else {
+        return "Not valid"
+    }
+}
+
+
+console.log(playRound(playerSelection,computerSelection))
