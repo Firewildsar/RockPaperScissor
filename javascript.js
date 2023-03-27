3b924ba2d5c1d6f0cbae8f844a31211c4ae58969
Firewildstar <122087233+Firewildsar@users.noreply.github.com>
2023-03-26 17:36:04 -0700

    Added 3 buttons for Rock Paper and Scissor. Displays the score line

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ store computer choice in variable choice
 let playerScore = 0;
 let computerScore = 0;
 let message = "";
-let result = ""
+let roundResult = ""
 
 function playerChoice(){
     let choice = prompt("Choose Rock, Paper, or Scissor");
@@ -35,64 +35,188 @@ function getComputerChoice(){
 
 
 function playRound(playerSelection,computerSelection){
+    const scoreLine = document.createElement('div');
+    scoreLine.classList.add('scoreLine');
+    container.appendChild(scoreLine);
+    const scoreResult = document.createElement('div');
+    scoreResult.classList.add('scoreResult');
+    scoreLine.appendChild(scoreResult);
+    
     if (playerSelection == "scissor"){
         if (computerSelection == "paper"){
+            const p = document.createElement('p');
+            p.innerText = 'You win! Scissor beats paper!';
+            scoreResult.appendChild(p);
             playerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You win! Scissor beats paper!"; 
         } else if (computerSelection == "rock"){
+            const p = document.createElement('p');
+            p.innerText = 'You lose! Rock beats scissor!';
+            scoreResult.appendChild(p);
             computerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You lose! Rock beats scissor!";
         } else {
+            const p = document.createElement('p');
+            p.innerText = 'You tie! Scissor does not beat scissor!';
+            scoreResult.appendChild(p);
             playerScore += 0;
             computerScore += 0;
-            roundResult = "You tie! Try again!";
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
+            roundResult = "You tie! Scissor does not beat scissor!";
         }
     }
     if (playerSelection == "rock"){
         if (computerSelection == "scissor"){
+            const p = document.createElement('p');
+            p.innerText = 'You win! Rock beats scissor!';
+            scoreResult.appendChild(p);
             playerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You win! Rock beats scissor!";
+           
         } else if (computerSelection == "paper"){
+            const p = document.createElement('p');
+            p.innerText = 'You lose! Paper beats rock!';
+            scoreResult.appendChild(p);
             computerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You lose! Paper beats rock!";
         } else {
+            const p = document.createElement('p');
+            p.innerText = 'You tie! Rock does not beat rock!';
+            scoreResult.appendChild(p);
             playerScore += 0;
             computerScore += 0;
-            roundResult = "You tie! Try again!";
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
+            roundResult = "You tie! Rock does not beat rock!";
         }
     }
     if (playerSelection == "paper"){
         if (computerSelection == "rock"){
+            const p = document.createElement('p');
+            p.innerText = 'You win! Paper beats rock!';
+            scoreResult.appendChild(p);
             playerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You win! Paper beats rock!";
         } else if (computerSelection == "scissor"){
+            const p = document.createElement('p');
+            p.innerText = 'You lose! Scissor beats paper!';
+            scoreResult.appendChild(p);
             computerScore += 1;
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
             roundResult = "You lose! Scissor beats paper!";
         } else {
+            const p = document.createElement('p');
+            p.innerText = 'You tie! Paper does not beat paper!';
+            scoreResult.appendChild(p);
             playerScore += 0;
             computerScore += 0;
-            roundResult = "You tie! Try again!";
+            const pScore = document.createElement('p');
+            pScore.innerText = `Player Score: ${playerScore}          Computer Score: ${computerScore}`;
+            scoreLine.appendChild(pScore);
+            roundResult = "You tie! Paper does not beat paper!";
         }
     }
-    console.log(roundResult)
-    console.log(computerScore)
-    console.log(playerScore)
+    if(playerScore === 5){
+        const h1 = document.createElement('h1');
+        h1.innerText = `Player wins! Score is ${playerScore} to ${computerScore}. Congratulations!`;
+        container.appendChild(h1);
+
+    } else if(computerScore === 5){
+        const h1 = document.createElement('h1');
+        h1.innerText = `Computer wins! Score is ${playerScore} to ${computerScore}. Better luck next time!`;
+        container.appendChild(h1);
+    }
     
-    return roundResult
+    console.log(roundResult)
+    console.log(playerScore)
+    console.log(computerScore)
+    
+    
     }
  
    
 function game(){
-    for (let i = 0; i < 5; i++){
-        if (i < 5){
-            let playerSelection = playerChoice();
+    
+            let playerSelection = 'rock';
             let computerSelection = getComputerChoice(); 
             playRound(playerSelection,computerSelection);
             console.log("works")
-    }
-}
+    
 }
 
-game()
 
+
+
+const container = document.querySelector('#container');
+const option = document.createElement('div');
+option.classList.add('option');
+const scoreResult = document.createElement('div');
+scoreResult.classList.add('scoreResult');
+
+
+const btnRock = document.createElement('button');
+btnRock.classList.add('rock');
+btnRock.textContent = 'ROCK';
+
+
+
+
+const btnPaper = document.createElement('button');
+btnPaper.classList.add('paper');
+btnPaper.textContent = 'PAPER';
+const btnScissor = document.createElement('button');
+btnScissor.classList.add('scissor');
+btnScissor.textContent = 'SCISSOR';
+
+
+
+
+btnRock.addEventListener('click', () =>{
+            let playerSelection = 'rock';
+            let computerSelection = getComputerChoice(); 
+            playRound(playerSelection,computerSelection);
+            console.log("works")
+});
+
+btnPaper.addEventListener('click', function (e){
+            let playerSelection = 'paper';
+            let computerSelection = getComputerChoice(); 
+            playRound(playerSelection,computerSelection);
+            console.log("works")
+})
+
+btnScissor.addEventListener('click', function (e){
+            let playerSelection = 'scissor';
+            let computerSelection = getComputerChoice(); 
+            playRound(playerSelection,computerSelection);
+            console.log("works")
+})
+
+
+
+
+option.appendChild(btnRock);
+option.appendChild(btnPaper);
+option.appendChild(btnScissor);
+container.appendChild(option);
 
